3d60a4c001fe1dd7299e24d5b8e761319979d1cc
Alexandre Schulz <a.schulz1992@gmail.com>
2022-09-20 15:47:04 +0200

    dataset generation

--- README.md
+++ README.md
@@ -3,43 +3,14 @@
 Python implementation of the method presented in the paper "MIP-GNN: A Data-Driven Framework for Guiding Combinatorial Solvers" by
 Khalil et al.
 
-## Pre trained models
+## Usage
+This package allows you to generate MIP datasets, train MIP-GNN models and evaluate their performance on collections of MIP instances.
+By default datasets are stored in `~/.cache/mipgnn/datasets` and models in `~/.cache/mipgnn/models`.
 
-Pretrained models are available in the original GitHub repository. They follow the naming convention : 
-<model_class>_<problem_class>_<problem_params>_<training_params>
+## Generating datasets
+As of today this package only allows you to generate instance of Set Covering problems. 
+```bash
+python3 -m mipgnn.generate_data <dataset_name> -n <num_instances> -V <variables> -c <constraints> -d <density>
+```
 
-## Submodules and objects
-
-MIPInstance : instance of a MIP, loaded from a .lp or .mps file
-  - load(file: FileObject, filename: str = None) : load a MIP instance from a .mps or .lp file
-  - dump(file, filename: str = None, format: str = ".mps") : dump MIP instance to .mps or .lp file
-  - get_graph() : get the corresponding MIPGraph instance
-
-MIPBGraph(networkx.Graph) : bipartite graph representing a MIP instance
-  - node attributes : 
-    - "type": in ["variable", "constraint"]
-    - "features": list of features of the node
-
-  - load(file: FileType, filename: str = None) : load a MIPGraph object from a file
-  - dump(file: FileType, filename: str = None) : dump MIPGraph object to file
-  - get_data() : get the MIPData instance corresponding to the graph
-
-MIPData(torch_geometric.data.Data) : object containing features for each node and edge in the MIP Bipartite graph representation of the problem
-  - variable_map : maps the networkx nodes to variable node ids
-  - constraint_map : maps the networkx nodes to constraint node ids
-  - variable_count : number of variables
-  - constraint_count : number of constraints
-  - variable_features : variable node features
-  - constraint_features : constraint node features
-  - constraint_rhs : constraint right hand side values
-  - objective : objective function
-  - variable_index : indexes of the variables nodes
-  - constraint_index : indexes of the constraint nodes
-  
-
-MIPGenerator : MIP instance generation
-
-- dataset : creating and loading datasets of MIP problems
-- training : training a model on a given dataset
-- networks : GNN model implementations
-- inference : executing the model
+## Training a model
--- mipgnn/generate_data.py
+++ mipgnn/generate_data.py
@@ -1,4 +1,3 @@
-import ecole
 import shutil
 import os
 import sys
@@ -9,46 +8,62 @@ import pyscipopt as pyscip
 import pickle as pkl
 import networkx as nx
 from tqdm import trange
+import argparse
+
+import ecole
+
+import mipgnn.configuration as config
 
 from mipgnn.utility import *
 from mipgnn import bias_search
-
 from mipgnn.instance import MIPInstance
 
-def generate_instances(name, n_constraint = [500], n_variable = [500], density = [0.2],n_instance = 1000, dataset_directory=os.path.expanduser("~/.cache/mipgnn/datasets"), generator=ecole.instance.SetCoverGenerator):
+def generate_instances(name, n_constraint=500, n_variable=500, density=0.2, n_instance=1000, dataset_directory=config.dataset_directory, generator=ecole.instance.SetCoverGenerator):
     path = os.path.join(dataset_directory, name)
-    #if os.path.exists(path):
-    #    shutil.rmtree(path)
+    if os.path.exists(path):
+        ans=input("Overwrite dataset in {path} ?(y/n)")
+        if ans=="n":
+            return
+        else:
+            shutil.rmtree(path)
     # create the dataset directory if it does not exist
     os.makedirs(path, exist_ok=True)
     
-    for row, col, d in itertools.product(n_constraint, n_variable, density):
-        instance_generator = generator(n_rows = row, n_cols = col,density = d) 
-        dataset_name = f"set_cover_{row}_{col}_{d}"
-        dataset_path = os.path.join(path, dataset_name)
-        if os.path.exists(dataset_path):
-            shutil.rmtree(dataset_path)
-        os.makedirs(dataset_path, exist_ok=True)
+    instance_generator = generator(n_rows=n_constraint, n_cols=n_variable, density=density) 
 
-        print("Generate with Row:%d,Col:%d,Density:%f" % (row,col,d))
-        for n in trange(1,n_instance+1):
-            # new instance
-            instance = next(instance_generator)
-            instance_path = os.path.join(dataset_path, f"{n}.mps")
-            instance.write_problem(instance_path)
-            # build the bipartite graph
-            mip_instance = MIPInstance.load(instance_path)
-            G = mip_instance.bipartite_graph()
-            graph_path = os.path.join(dataset_path, f"{n}_graph.pkl")
-            nx.write_gpickle(G, graph_path)
-            # compute biases
-            bias_search.search(instance_path, True)
+    print(f"Generating {n_instance} instances with n_var={n_variable}, n_constr={n_constraint}, density={density}.")
+    print(f"Saving in {path}")
+    for n in trange(1,n_instance+1):
+        # new instance
+        instance = next(instance_generator)
+        instance_path = os.path.join(path, f"{n}.mps")
+        instance.write_problem(instance_path)
+        # build the bipartite graph
+        mip_instance = MIPInstance.load(instance_path)
+        G = mip_instance.bipartite_graph()
+        graph_path = os.path.join(path, f"{n}_graph.pkl")
+        nx.write_gpickle(G, graph_path)
+        # compute biases
+        bias_search.search(instance_path, True)
 
 
 
 if __name__=="__main__":
-    generate_instances("test_dataset", 
-            n_constraint=[30], 
-            n_variable=[30], 
-            density=[0.1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", type=str, help="dataset name, files will be save in config.dataset_directory")
+    parser.add_argument("-n", "--instances", type=int, help="number of instances (default=1000)", default=1000)
+    parser.add_argument("-V", "--variable-count", type=int, help="number of variables (default=100)", default=100)
+    parser.add_argument("-c", "--constraint-count", type=int, help="number of constraints (default=100)", default=100)
+    parser.add_argument("-d", "--density", type=float, help="graph density (default=.1)", default=.1)
+    parser.add_argument("-D", "--output-dir", type=str, help="output directory, default is config.dataset_directory", default=config.dataset_directory)
+
+
+    args = parser.parse_args()
+
+    generate_instances(args.name, 
+            n_constraint=args.constraint_count, 
+            n_variable=args.variable_count, 
+            density=args.density,
+            n_instance=args.instances,
+            dataset_directory=args.output_dir
             )
